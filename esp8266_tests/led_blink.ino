f9e7a4aa5eaa69278a946b26c3a11f76f29fa77c
smashpl <krzysztof.bialkowski@gmail.com>
2016-04-09 20:53:17 +0200

    esp led blink

---
+++ esp8266_tests/led_blink.ino
@@ -0,0 +1,20 @@
+int outPin = 13; //on board D7
+
+void setup() {
+
+  pinMode(outPin,OUTPUT);
+  Serial.begin(115200);  
+  Serial.println("Start...");
+  delay(2000);
+}
+
+void loop() { 
+   
+  digitalWrite(outPin,HIGH);//state high to output pin
+  Serial.println("HIGH"); //just a comment
+  delay(1000); //some delay to see light
+  digitalWrite(outPin,LOW);;//state low to output pin
+  Serial.println("LOW");  
+  delay(1000);
+  
+}
