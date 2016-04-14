6d284bb4d715dd8103a5439a761c7e9b5a93b44d
smashpl <krzysztof.bialkowski@gmail.com>
2016-04-14 23:11:50 +0200

    pir movement sensor HC-SR501

---
+++ esp8266_tests/pir_sensor/pir_sensor.ino
@@ -0,0 +1,51 @@
+
+int pirPin = 5;    //PIR pin
+int ledPin = 13;   //led pin
+int prevState = LOW;
+int state = LOW;
+int i = 0;
+
+/////////////////////////////
+//SETUP
+void setup(){
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  Serial.println("Waiting for module to settle...");  
+  state = digitalRead(pirPin);  
+
+  //Loop util state is LOW
+  while(state==HIGH){
+    state = digitalRead(pirPin);    
+    Serial.print(i);
+    Serial.print(" state:" );
+    Serial.print(state);
+    Serial.println("");
+    delay(1000);
+    i++;
+  }
+}
+
+////////////////////////////
+void loop(){
+  
+  state = digitalRead(pirPin);
+
+  if(state==HIGH and prevState==LOW){
+    Serial.println("Move detected !!!");  
+    prevState=HIGH;
+    digitalWrite(ledPin,state);
+    i=0;
+  }
+
+  if(state==LOW and prevState==HIGH){
+    Serial.println("Waiting...");
+    prevState=LOW;    
+    digitalWrite(ledPin,state);
+    i=0;
+  }   
+  Serial.println(i);
+  i++;
+  delay(1000);
+}
